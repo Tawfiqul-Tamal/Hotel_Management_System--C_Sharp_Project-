b79057a12869c051cac049771cb1fe3ef12d7b77
Md. Tawfiqul Islam Tamal <146660961+Tawfiqul-Tamal@users.noreply.github.com>
2026-03-02 00:12:30 +0600

    Add files via upload

---
+++ HotelManagement/HotelManagement/AdminChangePasswordForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace HotelManagement
+{
+    partial class AdminChangePasswordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtCurrPass = new System.Windows.Forms.TextBox();
+            this.txtNewPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNewPassword = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(123, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Change Password Form";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(287, 224);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(146, 31);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Change Password";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // txtCurrPass
+            // 
+            this.txtCurrPass.Location = new System.Drawing.Point(225, 141);
+            this.txtCurrPass.Name = "txtCurrPass";
+            this.txtCurrPass.Size = new System.Drawing.Size(208, 20);
+            this.txtCurrPass.TabIndex = 9;
+            this.txtCurrPass.UseSystemPasswordChar = true;
+            // 
+            // txtNewPass
+            // 
+            this.txtNewPass.Location = new System.Drawing.Point(225, 181);
+            this.txtNewPass.Name = "txtNewPass";
+            this.txtNewPass.Size = new System.Drawing.Size(208, 20);
+            this.txtNewPass.TabIndex = 10;
+            this.txtNewPass.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 22);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Current Password:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNewPassword
+            // 
+            this.lblNewPassword.AutoSize = true;
+            this.lblNewPassword.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewPassword.Location = new System.Drawing.Point(82, 181);
+            this.lblNewPassword.Name = "lblNewPassword";
+            this.lblNewPassword.Size = new System.Drawing.Size(137, 22);
+            this.lblNewPassword.TabIndex = 12;
+            this.lblNewPassword.Text = "New Password:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(162, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 31);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AdminChangePasswordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(515, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblNewPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNewPass);
+            this.Controls.Add(this.txtCurrPass);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label3);
+            this.Name = "AdminChangePasswordForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdminChangePasswordForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtCurrPass;
+        private System.Windows.Forms.TextBox txtNewPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNewPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
